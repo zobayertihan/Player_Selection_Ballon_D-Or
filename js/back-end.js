591caa8231291bb891c80f8e7473d97ef7b091ce
zobayertihan <zobayertihan@gmail.com>
2022-08-22 02:44:18 +0600

    third

--- js/back-end.js
+++ js/back-end.js
@@ -1,18 +1,71 @@
-let numb = document.getElementById("player-list").childElementCount;
-console.log(numb)
+function listCount() {
+    let numb = document.getElementById("player-list").childElementCount;
+    return numb;
+}
+function addNameToList(getElement) {
+    const playerList = document.getElementById("player-list");
+    getPlayerName = document.getElementById(getElement);
+    playerName = getPlayerName.innerText;
+    const playerNameString = document.createElement('li');
+    playerNameString.appendChild(document.createTextNode(playerName));
+    playerList.appendChild(playerNameString);
+}
 
 document.getElementById('player-1').addEventListener('click', function () {
-    getPlayerName = document.getElementById('messi-1');
-    playerName = getPlayerName.innerText;
-    getList = document.getElementById('list-1');
-    if (numb > 0) {
-        getList.innerText = playerName;
-        document.getElementById("player-1").disabled = true;
-        numb = numb - 1;
+    if (listCount() > 4) {
+        alert("Cant");
     }
     else {
-        alert("Can not select more then 5");
+        addNameToList('messi-1');
+        document.getElementById("player-1").disabled = true;
     }
-    console.log(numb);
+})
+document.getElementById('player-2').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant");
+    }
+    else {
+        addNameToList('messi-2');
+        document.getElementById("player-2").disabled = true;
+    }
 })
 
+document.getElementById('player-3').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant");
+    }
+    else {
+        addNameToList('messi-3');
+        document.getElementById("player-3").disabled = true;
+    }
+})
+
+document.getElementById('player-4').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant");
+    }
+    else {
+        addNameToList('messi-4');
+        document.getElementById("player-4").disabled = true;
+    }
+})
+
+document.getElementById('player-5').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant");
+    }
+    else {
+        addNameToList('messi-5');
+        document.getElementById("player-5").disabled = true;
+    }
+})
+
+document.getElementById('player-6').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant");
+    }
+    else {
+        addNameToList('messi-6');
+        document.getElementById("player-6").disabled = true;
+    }
+})
