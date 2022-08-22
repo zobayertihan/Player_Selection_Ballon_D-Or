4f982bb839c5750a5e7bc0dda1e93a8705274450
zobayertihan <zobayertihan@gmail.com>
2022-08-22 23:19:36 +0600

    sixth

--- js/back-end.js
+++ js/back-end.js
@@ -20,79 +20,117 @@ function getElementValue(elementId) {
 
 document.getElementById('player-1').addEventListener('click', function () {
     if (listCount() > 4) {
-        alert("Cant");
+        alert("Cant add more then Five Players");
     }
     else {
-        addNameToList('messi-1');
+        addNameToList('benzema');
         document.getElementById("player-1").disabled = true;
     }
 })
 document.getElementById('player-2').addEventListener('click', function () {
     if (listCount() > 4) {
-        alert("Cant");
+        alert("Cant add more then Five Players");
     }
     else {
-        addNameToList('messi-2');
+        addNameToList('courtois');
         document.getElementById("player-2").disabled = true;
     }
 })
 
 document.getElementById('player-3').addEventListener('click', function () {
     if (listCount() > 4) {
-        alert("Cant");
+        alert("Cant add more then Five Players");
     }
     else {
-        addNameToList('messi-3');
+        addNameToList('ronaldo');
         document.getElementById("player-3").disabled = true;
     }
 })
 
 document.getElementById('player-4').addEventListener('click', function () {
     if (listCount() > 4) {
-        alert("Cant");
+        alert("Cant add more then Five Players");
     }
     else {
-        addNameToList('messi-4');
+        addNameToList('casemiro');
         document.getElementById("player-4").disabled = true;
     }
 })
 
 document.getElementById('player-5').addEventListener('click', function () {
     if (listCount() > 4) {
-        alert("Cant");
+        alert("Cant add more then Five Players");
     }
     else {
-        addNameToList('messi-5');
+        addNameToList('modric');
         document.getElementById("player-5").disabled = true;
     }
 })
 
 document.getElementById('player-6').addEventListener('click', function () {
     if (listCount() > 4) {
-        alert("Cant");
+        alert("Cant add more then Five Players");
     }
     else {
-        addNameToList('messi-6');
+        addNameToList('vini');
         document.getElementById("player-6").disabled = true;
     }
 })
 
+document.getElementById('player-7').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant add more then Five Players");
+    }
+    else {
+        addNameToList('rudiger');
+        document.getElementById("player-7").disabled = true;
+    }
+})
+
+document.getElementById('player-8').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant add more then Five Players");
+    }
+    else {
+        addNameToList('kevin');
+        document.getElementById("player-8").disabled = true;
+    }
+})
+
+document.getElementById('player-9').addEventListener('click', function () {
+    if (listCount() > 4) {
+        alert("Cant add more then Five Players");
+    }
+    else {
+        addNameToList('son');
+        document.getElementById("player-9").disabled = true;
+    }
+})
+
+
 document.getElementById('calculate').addEventListener('click', function () {
     const val = listCount();
-
-    getElementValue('per-player');
-
+    let perPlayer = getElementValue('per-player');
+    if (isNaN(perPlayer)) {
+        alert('Please Enter a Number');
+        return;
+    }
     const getPlayerExpenses = document.getElementById('player-expenses');
-    const playerExpenses = val * getElementValue('per-player');
+    const playerExpenses = val * perPlayer;
     getPlayerExpenses.value = playerExpenses;
 })
 
 document.getElementById('total-calculate').addEventListener('click', function () {
-    getElementValue('manager');
-    getElementValue('coach');
     const val = listCount();
-    const playerExpenses = val * getElementValue('per-player');
-    const totalExpenses = playerExpenses + getElementValue('manager') + getElementValue('coach');
+    let perPlayer = getElementValue('per-player');
+    let manager = getElementValue('manager');
+    let coach = getElementValue('coach');
+    if (isNaN(manager && coach)) {
+        alert('Please Enter a Number');
+        return;
+    }
+    const playerExpenses = val * perPlayer;
+    const totalExpenses = playerExpenses + manager + coach;
     const getTotalExpeses = document.getElementById('total-expenses')
     getTotalExpeses.value = totalExpenses;
 
